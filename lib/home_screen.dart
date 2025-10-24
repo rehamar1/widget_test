5bb126fd6fbebcb8c6e746f994fc3d1c674887b0
rehamar1 <arjouniryhem@gmail.com>
2025-10-24 16:14:10 +0100

    use a listView horizontal

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -19,7 +19,32 @@ class _MyHomePageState extends State<MyHomePage> {
 
         title: Text(widget.title),
       ),
-      body: Column(children: [CardProfile()]),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(14),
+            child: Text(
+              'List horizontal : ',
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(
+            height: 180,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 5,
+              padding: EdgeInsets.all(14),
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: EdgeInsets.only(right: 5),
+                  child: CardProfile(),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
